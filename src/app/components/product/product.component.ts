98b1ae6722b9a61fcc585e4e1087cf8aa1455f7d
CarlosDantas <carlosdantas485@gmail.com>
2023-07-05 15:34:48 -0300

    (GET) info by API

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { API_CONFIG } from 'src/app/config/api.config';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  products = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,]
 
+  ELEMENT_DATA: Product[] = []
+  constructor(
+    private http: HttpClient
+    ){}
+
+    ngAfterViewInit() {
+      this.getAll()
+    }
+
+    getAll(){
+      this.http.get<Product[]>(`${API_CONFIG.baseUrl}products`).subscribe(response => {
+        response
+        this.ELEMENT_DATA = response
+        
+        console.log(response)
+      })
+    }
 }
---
+++ src/app/models/Product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    id?: any;
+    name: string;
+    productType: string[]
+    description:string;
+    img: string;
+    oldPrice: number;
+    newPrice: number;
+
+}
